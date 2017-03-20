5bc3db9c2276aa4151554cd8464c02525c127e22
Jules Bonnard <julesbonnard@gmail.com>
2017-03-20 02:46:27 +0100

    fix dist

--- config/base.js
+++ config/base.js
@@ -11,7 +11,7 @@ module.exports = () => {
             path: path.resolve(__dirname, '..', 'dist'),
             filename: '[name].[chunkhash].js',
             sourceMapFilename: '[name].map',
-            publicPath: '/'
+            publicPath: './'
         },
         module: {
             rules: [{
